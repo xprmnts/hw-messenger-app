87e0572056047195e57523a5e79959fe8e98f691
xprmnts <abhinay@outlook.com>
2021-05-11 15:49:07 -0400

    refactored branding such that it is in a seperate component

--- client/src/components/Auth/Auth.js
+++ client/src/components/Auth/Auth.js
@@ -7,42 +7,14 @@ import {
     Typography,
     FormControl,
     TextField,
-    FormHelperText,
-    SvgIcon
+    FormHelperText
 } from '@material-ui/core';
 import { withStyles } from '@material-ui/core/styles';
 import { register, login } from '../../store/utils/thunkCreators';
 import AuthButton from './AuthButton';
-import BackgroundImage from '../../assets/images/bg-img.png';
-import { ReactComponent as Logo } from '../../assets/images/bubble.svg';
+import AuthBranding from './AuthBranding';
 
 const styles = theme => ({
-    imageContainer: {
-        height: '100vh',
-        backgroundRepeat: 'no-repeat',
-        backgroundImage: `url(${BackgroundImage})`,
-        backgroundSize: 'cover',
-        backgroundPosition: 'center'
-    },
-    imageOverlay: {
-        background:
-            'linear-gradient(to bottom, rgba(58, 141, 255, 0.85), rgba(134, 185, 255, 0.85));',
-        height: '100vh',
-        display: 'flex',
-        flexDirection: 'column',
-        alignItems: 'center',
-        justifyContent: 'center'
-    },
-    iconRoot: {
-        width: '6rem',
-        height: '6rem',
-        marginTop: '-15rem'
-    },
-    tagLine: {
-        fontSize: '2rem',
-        marginTop: '4rem',
-        color: '#FFF'
-    },
     formGrid: {
         flex: 1
     },
@@ -154,33 +126,7 @@ const Auth = props => {
 
     return (
         <Grid container component='main'>
-            <Grid
-                item
-                xs={false}
-                sm={4}
-                md={6}
-                className={props.classes.imageContainer}
-            >
-                <Box display={{ xs: 'none', sm: 'block' }}>
-                    <div className={props.classes.imageOverlay}>
-                        <SvgIcon
-                            viewBox='0 0 67 67'
-                            className={props.classes.iconRoot}
-                        >
-                            <Logo />
-                        </SvgIcon>
-                        <Typography
-                            align='center'
-                            className={props.classes.tagLine}
-                        >
-                            Converse with anyone
-                            <br />
-                            with any language
-                        </Typography>
-                    </div>
-                </Box>
-            </Grid>
-
+            <AuthBranding />
             <Grid
                 item
                 container
@@ -189,24 +135,6 @@ const Auth = props => {
                 justify='center'
             >
                 <Box className={props.classes.formWrapper}>
-                    <Box
-                        display={{ xs: 'none', sm: 'block' }}
-                        className={props.classes.secondaryCTAWrapper}
-                    >
-                        <Grid
-                            container
-                            item
-                            className={props.classes.secondaryCTAContainer}
-                        >
-                            <Typography color='secondary'>
-                                {secondaryCTAText}
-                            </Typography>
-
-                            <AuthButton onClick={secondaryCTARouteHandler}>
-                                {secondaryCTAButtonText}
-                            </AuthButton>
-                        </Grid>
-                    </Box>
                     <Box className={props.classes.formMainContainer}>
                         <Typography className={props.classes.formTitle}>
                             {formTitleTypography}
---
+++ client/src/components/Auth/AuthBranding.js
@@ -0,0 +1,67 @@
+import React from 'react';
+import { Grid, Box, Typography, SvgIcon } from '@material-ui/core';
+import { withStyles } from '@material-ui/core/styles';
+import BackgroundImage from '../../assets/images/bg-img.png';
+import { ReactComponent as Logo } from '../../assets/images/bubble.svg';
+
+const styles = theme => ({
+    imageContainer: {
+        height: '100vh',
+        backgroundRepeat: 'no-repeat',
+        backgroundImage: `url(${BackgroundImage})`,
+        backgroundSize: 'cover',
+        backgroundPosition: 'center'
+    },
+    imageOverlay: {
+        background:
+            'linear-gradient(to bottom, rgba(58, 141, 255, 0.85), rgba(134, 185, 255, 0.85));',
+        height: '100vh',
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    iconRoot: {
+        width: '6rem',
+        height: '6rem',
+        marginTop: '-15rem'
+    },
+    tagLine: {
+        fontSize: '2rem',
+        marginTop: '4rem',
+        color: '#FFF'
+    }
+});
+
+const AuthBranding = props => {
+    return (
+        <Grid
+            item
+            xs={false}
+            sm={4}
+            md={6}
+            className={props.classes.imageContainer}
+        >
+            <Box display={{ xs: 'none', sm: 'block' }}>
+                <div className={props.classes.imageOverlay}>
+                    <SvgIcon
+                        viewBox='0 0 67 67'
+                        className={props.classes.iconRoot}
+                    >
+                        <Logo />
+                    </SvgIcon>
+                    <Typography
+                        align='center'
+                        className={props.classes.tagLine}
+                    >
+                        Converse with anyone
+                        <br />
+                        with any language
+                    </Typography>
+                </div>
+            </Box>
+        </Grid>
+    );
+};
+
+export default withStyles(styles)(AuthBranding);
