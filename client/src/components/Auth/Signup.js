20ed0d4d7fc5d788c980e7b7bdb818ec7238b0aa
xprmnts <abhinay@outlook.com>
2021-05-11 11:02:53 -0400

    added background image and adjusted layout to be responsive, image doesn't show on sm screens

--- client/src/components/Auth/Signup.js
+++ client/src/components/Auth/Signup.js
@@ -10,8 +10,22 @@ import {
     FormHelperText
 } from '@material-ui/core';
 import { withStyles } from '@material-ui/core/styles';
-import AuthButton from './AuthButton';
 import { register } from '../../store/utils/thunkCreators';
+import AuthButton from './AuthButton';
+import BackgroundImage from '../../assets/images/bg-img.png';
+
+const styles = {
+    backgroundContainer: {
+        height: '100vh',
+        backgroundRepeat: 'no-repeat',
+        backgroundImage: `url(${BackgroundImage})`,
+        backgroundSize: 'cover',
+        backgroundPosition: 'center'
+    },
+    formContainer: {
+        flex: 1
+    }
+};
 
 const Signup = props => {
     const history = useHistory();
@@ -38,76 +52,90 @@ const Signup = props => {
     }
 
     return (
-        <Grid container justify='center'>
-            <Box>
-                <Grid container item>
-                    <Typography>Need to log in?</Typography>
-                    <AuthButton onClick={() => history.push('/login')}>
-                        Login
-                    </AuthButton>
-                </Grid>
-                <form onSubmit={handleRegister}>
-                    <Grid>
-                        <Grid>
-                            <FormControl>
-                                <TextField
-                                    aria-label='username'
-                                    label='Username'
-                                    name='username'
-                                    type='text'
-                                    required
-                                />
-                            </FormControl>
-                        </Grid>
-                        <Grid>
-                            <FormControl>
-                                <TextField
-                                    label='E-mail address'
-                                    aria-label='e-mail address'
-                                    type='email'
-                                    name='email'
-                                    required
-                                />
-                            </FormControl>
-                        </Grid>
-                        <Grid>
-                            <FormControl
-                                error={!!formErrorMessage.confirmPassword}
-                            >
-                                <TextField
-                                    aria-label='password'
-                                    label='Password'
-                                    type='password'
-                                    inputProps={{ minLength: 6 }}
-                                    name='password'
-                                    required
-                                />
-                                <FormHelperText>
-                                    {formErrorMessage.confirmPassword}
-                                </FormHelperText>
-                            </FormControl>
-                        </Grid>
-                        <Grid>
-                            <FormControl
-                                error={!!formErrorMessage.confirmPassword}
-                            >
-                                <TextField
-                                    label='Confirm Password'
-                                    aria-label='confirm password'
-                                    type='password'
-                                    inputProps={{ minLength: 6 }}
-                                    name='confirmPassword'
-                                    required
-                                />
-                                <FormHelperText>
-                                    {formErrorMessage.confirmPassword}
-                                </FormHelperText>
-                            </FormControl>
-                        </Grid>
-                        <AuthButton type='submit'>Create</AuthButton>
+        <Grid container component='main'>
+            <Grid
+                item
+                xs={false}
+                sm={4}
+                md={6}
+                className={props.classes.backgroundContainer}
+            />
+
+            <Grid
+                container
+                className={props.classes.formContainer}
+                justify='center'
+            >
+                <Box>
+                    <Grid container item>
+                        <Typography>Need to log in?</Typography>
+                        <AuthButton onClick={() => history.push('/login')}>
+                            Login
+                        </AuthButton>
                     </Grid>
-                </form>
-            </Box>
+                    <form onSubmit={handleRegister}>
+                        <Grid>
+                            <Grid>
+                                <FormControl>
+                                    <TextField
+                                        aria-label='username'
+                                        label='Username'
+                                        name='username'
+                                        type='text'
+                                        required
+                                    />
+                                </FormControl>
+                            </Grid>
+                            <Grid>
+                                <FormControl>
+                                    <TextField
+                                        label='E-mail address'
+                                        aria-label='e-mail address'
+                                        type='email'
+                                        name='email'
+                                        required
+                                    />
+                                </FormControl>
+                            </Grid>
+                            <Grid>
+                                <FormControl
+                                    error={!!formErrorMessage.confirmPassword}
+                                >
+                                    <TextField
+                                        aria-label='password'
+                                        label='Password'
+                                        type='password'
+                                        inputProps={{ minLength: 6 }}
+                                        name='password'
+                                        required
+                                    />
+                                    <FormHelperText>
+                                        {formErrorMessage.confirmPassword}
+                                    </FormHelperText>
+                                </FormControl>
+                            </Grid>
+                            <Grid>
+                                <FormControl
+                                    error={!!formErrorMessage.confirmPassword}
+                                >
+                                    <TextField
+                                        label='Confirm Password'
+                                        aria-label='confirm password'
+                                        type='password'
+                                        inputProps={{ minLength: 6 }}
+                                        name='confirmPassword'
+                                        required
+                                    />
+                                    <FormHelperText>
+                                        {formErrorMessage.confirmPassword}
+                                    </FormHelperText>
+                                </FormControl>
+                            </Grid>
+                            <AuthButton type='submit'>Create</AuthButton>
+                        </Grid>
+                    </form>
+                </Box>
+            </Grid>
         </Grid>
     );
 };
@@ -126,4 +154,7 @@ const mapDispatchToProps = dispatch => {
     };
 };
 
-export default connect(mapStateToProps, mapDispatchToProps)(Signup);
+export default connect(
+    mapStateToProps,
+    mapDispatchToProps
+)(withStyles(styles)(Signup));
