e6af8329e8c2285d78520f8885b08d3412cdc941
xprmnts <abhinay@outlook.com>
2021-05-12 12:25:35 -0400

    updated ui css so that if there's many messages the chat container overflows nicely as opposed to pushing the chat input to the very bottom

--- client/src/components/ActiveChat/Messages.js
+++ client/src/components/ActiveChat/Messages.js
@@ -1,24 +1,44 @@
-import React from "react";
-import { Box } from "@material-ui/core";
-import { SenderBubble, OtherUserBubble } from "../ActiveChat";
-import moment from "moment";
+import React from 'react';
+import { Box } from '@material-ui/core';
+import { withStyles } from '@material-ui/core/styles';
+import { SenderBubble, OtherUserBubble } from '../ActiveChat';
+import moment from 'moment';
 
-const Messages = (props) => {
-  const { messages, otherUser, userId } = props;
-
-  return (
-    <Box>
-      {messages.map((message) => {
-        const time = moment(message.createdAt).format("h:mm");
-
-        return message.senderId === userId ? (
-          <SenderBubble key={message.id} text={message.text} time={time} />
-        ) : (
-          <OtherUserBubble key={message.id} text={message.text} time={time} otherUser={otherUser} />
-        );
-      })}
-    </Box>
-  );
+const styles = {
+    root: {
+        overflowY: 'auto',
+        scrollbarWidth: 'none',
+        '&::-webkit-scrollbar': {
+            display: 'none'
+        }
+    }
 };
 
-export default Messages;
+const Messages = props => {
+    const { messages, otherUser, userId } = props;
+
+    return (
+        <Box className={props.classes.root}>
+            {messages.map(message => {
+                const time = moment(message.createdAt).format('h:mm');
+
+                return message.senderId === userId ? (
+                    <SenderBubble
+                        key={message.id}
+                        text={message.text}
+                        time={time}
+                    />
+                ) : (
+                    <OtherUserBubble
+                        key={message.id}
+                        text={message.text}
+                        time={time}
+                        otherUser={otherUser}
+                    />
+                );
+            })}
+        </Box>
+    );
+};
+
+export default withStyles(styles)(Messages);
