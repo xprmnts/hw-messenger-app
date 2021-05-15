396f4bd1b844bf24d7d31783c4b87900c3399290
xprmnts <abhinay@outlook.com>
2021-05-15 14:54:31 -0400

    updated socket message event handler by firt making sure each user joins a room designated by their own id, and when forwarding messages we send messages to the users room via their id (previously we were sending using their socket id

--- server/sockets/socketMessageHandler.js
+++ server/sockets/socketMessageHandler.js
@@ -1,20 +1,6 @@
-const socketMessageHandler = (io, socket) => {
+const socketMessageHandler = (socket) => {
   return ({ data, to }) => {
-    console.log('calling socket messang handler is ', socket.userId);
-    let recipientSocketId;
-    // get the socket id of the recipient
-    for (let [id, socket] of io.of('/').sockets) {
-      if (socket.userId === to) {
-        recipientSocketId = id;
-      }
-    }
-
-    // if recipient is online emit a message to them via their private room
-    // by default socket provides every socket an id which also behaves as the
-    // pointer to a private room which the user is already a part of
-    if (recipientSocketId) {
-      socket.to(recipientSocketId).emit('new-message', data);
-    }
+    socket.to(to).emit('new-message', data);
   };
 };
 
