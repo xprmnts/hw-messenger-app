480180f1c243822d002db95081a61e5e5a738836
xprmnts <abhinay@outlook.com>
2021-05-16 10:04:55 -0400

    update socket read status handler to use improved sockets implementation

--- server/sockets/socketReadStatusHandler.js
+++ server/sockets/socketReadStatusHandler.js
@@ -1,20 +1,6 @@
 const socketReadStatusHandler = (io, socket) => {
-  return ({ messages, to }) => {
-    console.log('sending read to ', to);
-    let recipientSocketId;
-    // get the socket id of the recipient
-    for (let [id, socket] of io.of('/').sockets) {
-      if (socket.userId === to) {
-        recipientSocketId = id;
-      }
-    }
-
-    // if recipient is online emit a message to them via their private room
-    // by default socket provides every socket an id which also behaves as the
-    // pointer to a private room which the user is already a part of
-    if (recipientSocketId) {
-      socket.to(recipientSocketId).emit('read-messages', messages);
-    }
+  return ({ message, to }) => {
+    socket.to(to).to(socket.userId).emit('read-messages', message);
   };
 };
 
