c316bf57050bd3e8796f0b09c0c1d5078a1a0e4a
xprmnts <abhinay@outlook.com>
2021-05-18 16:28:23 -0400

    update pg connection for ssl

--- db/db.js
+++ db/db.js
@@ -1,12 +1,14 @@
-const pg = require('pg');
-pg.defaults.ssl = true;
-
 const Sequelize = require('sequelize');
 
 const db = new Sequelize(
     process.env.DATABASE_URL || 'postgres://localhost:5432/messenger',
     {
-        logging: false
+        dialect: 'postgres',
+        protocol: 'postgres',
+        dialectOptions: {
+            ssl: true,
+            rejectUnauthorized: false
+        }
     }
 );
 
